0e841581b43990ee918d9ad602667822a6a581ea
Andreas Schawo <andreas@schawo.de>
2025-12-09 20:41:02 +0100

    Added solution for day 6/7

--- days/day-06-07/README.md
+++ days/day-06-07/README.md
@@ -4,3 +4,10 @@ Day 6/7
 The 6th and 7th days features a Hot Wire game.
 
 Therefore, some sound is added.
+
+
+What I have learned
+-------------------
+
+- Using the PAC abstraction layer
+- Using the internal timer of the microcontroller
---
+++ days/day-06-07/src/main.rs
@@ -0,0 +1,38 @@
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use embedded_hal::delay::DelayNs;
+use embedded_hal::digital::{InputPin, OutputPin};
+use nrf52833_hal::{gpio, pac, timer};
+use panic_halt as _;
+use rtt_target::{rprintln, rtt_init_print};
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("Initializing...");
+    let peripherals = pac::Peripherals::take().unwrap();
+    let p0 = gpio::p0::Parts::new(peripherals.P0);
+    let mut timer_f = timer::Timer::new(peripherals.TIMER0);
+
+    // Ring 0
+    let mut hot_wire = p0.p0_02.into_pullup_input();
+    // Ring 1
+    let mut speaker = p0.p0_00.into_push_pull_output(gpio::Level::Low);
+
+    let freq = 420;
+    let period = 1000u32 / freq;
+
+    rprintln!("Running...");
+    loop {
+        if hot_wire.is_low().unwrap() {
+            for _ in 0..1000 / period {
+                timer_f.delay_ms(period / 2);
+                speaker.set_high().unwrap();
+                timer_f.delay_ms(period / 2);
+                speaker.set_low().unwrap();
+            }
+        }
+    }
+}
